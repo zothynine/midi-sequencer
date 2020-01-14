5338fdc13843670557ec8f3bec7cbb3d4f32aa75
Mario Zoth <mario.zoth@icloud.com>
2020-01-14 21:55:14 +0100

    Add midi api

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Hello sequencer!");
-});
+if (navigator.requestMIDIAccess) {
+
+    navigator.requestMIDIAccess({
+        sysex: false // this defaults to 'false' and we won't be covering sysex in this article. 
+    }).then(onMIDISuccess, onMIDIFailure)
+} else {
+
+    alert("No MIDI support in your browser.")
+}
+
+function onMIDISuccess(midiAccess) {
+    
+    console.log('MIDI Access Object', midiAccess)
+}
+
+function onMIDIFailure(e) {
+
+    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e)
+}
